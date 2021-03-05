acdbe2e9a152eb6351eacde4e208d5776aaf4ae2
Shiyi Peng <shiyipeng@microsoft.com>
2021-03-04 21:33:17 -0800

    use main DO branch for now

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -45,7 +45,7 @@ catch2_ref=$default_catch2_ref
 
 # DO Deps
 install_do=false
-default_do_ref=0.6.1
+default_do_ref=main
 do_ref=$default_do_ref
 install_do_deps=false
 default_do_deps_distro=ubuntu1804
